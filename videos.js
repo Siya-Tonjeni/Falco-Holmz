8efc199f540f3f6b6214daebecf16e2410adef99
Siya-Tonjeni <tonjenisiya@gmail.com>
2025-05-28 14:41:14 +0200

    Updates

--- videos.js
+++ videos.js
@@ -1,4 +1,26 @@
-function changeVideo(videoId) {
-    let mainVideo = document.getElementById("main-video");
-    mainVideo.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-}
+function changeVideo(videoId, clickedItem) {
+    const mainVideoWrapper = document.querySelector('.main-video');
+    const oldIframe = document.getElementById("main-video");
+  
+    if (oldIframe) oldIframe.remove();
+  
+    const newIframe = document.createElement('iframe');
+    newIframe.id = "main-video";
+    newIframe.width = "100%";
+    newIframe.height = "450";
+    newIframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    newIframe.frameBorder = "0";
+    newIframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    newIframe.allowFullscreen = true;
+  
+    mainVideoWrapper.appendChild(newIframe);
+  
+    const allItems = document.querySelectorAll('.video-item');
+    allItems.forEach(item => item.classList.remove('active'));
+  
+    if (clickedItem) {
+      clickedItem.classList.add('active');
+    }
+  }
+  
+  
